a8f73e2448f7d0da2fd05bbce06761390d84214c
Sharp0802 <sharp0802.business@gmail.com>
2025-07-31 08:10:45 +0900

    :wrench: Add test project

---
+++ test/Foo.cs
@@ -0,0 +1,13 @@
+﻿namespace NativeExposer.Test;
+
+public partial class Foo
+{
+    private int _i;
+    
+    [Export]
+    private int Bar(int a, int b)
+    {
+        Console.WriteLine("Hello from C#");
+        return _i += a * b;
+    }
+}
